4b350e2871e5e26238484d9ea2bd87c2966a6020
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-13 14:32:47 -0800

    WIP: adjust functionality of submit button

--- js/styles.js
+++ js/styles.js
@@ -1,31 +1,24 @@
 window.onload = function() {
   let form = document.querySelector("form");
 
-  form.onsubmit = function(event) {
-    event.preventDefault();
+ function handleSelect(event) {
+  event.preventDefault();
 
-    let celebrity = document.getElementById("#celebrity")
-    let media = document.getElementById("media").value();
-    let direction = document.getElementById("direction");
-    let genre = document.getElementById("genre");
-    let color = document.getElementById("color");
+    const celebrity = document.getElementById("#celebrity")
+    let media = document.getElementById("#media")
+    let direction = document.getElementById("#direction");
+    let genre = document.getElementById("#genre");
+    let color = document.getElementById("#color");
 
     const userInput = document.getElementById("celebrity").value;
 
-    if ((celebrity === "N/A") || (direction == "N/A") || (color === "N/A")) {
-      alert("INCOMPLETE!")
-    }
+  }
 
-    if ((celebrity === "Willem Dafoe") || (media === "movies") || (direction == "up") || (color = "purple")) {
-      form.removeAttribute("class");
-    } else if ((celebrity === "Beyonce") || (media === "Video Games") || (direction == "down") || (color === "pink")) {
-      document.getElementById("C#").show();
-    } else if ((celebrity === "Wendy Williams") && (media === "television") || (color === "red")) {
-      form.removeAttribute("class")
-    } else {
-      form.removeAttribute("class")
+    if (celebrity === "robert") {
+    window.alert("robert")
     }
     
-
-  }
+window.addEventListener("load", function() {
+  document.getElementById("select-form").addEventListener("submit", handleSelect);
+  });
 }
