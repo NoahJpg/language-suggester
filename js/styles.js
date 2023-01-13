55e2f8baad3a17f3e58353a2c88534b6e20e11c0
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-13 09:48:54 -0800

    add more questions, add bootstrap, organize code

--- js/styles.js
+++ js/styles.js
@@ -7,6 +7,13 @@
 let form = document.querySelector("form");
 form.onsubmit = function(event) {
   event.preventDefault();
+
+
+  var celebrity = document.querySelector("input#celebrity").value;
+  var zodiac = document.querySelector("input#zodiac").value;
+  var direction = document.querySelector("input#direction").value;
+  var 
+
   const userInput = document.getElementById("celebrity").value;
 
   console.log("submit form successful")
