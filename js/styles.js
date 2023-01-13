3b63adf16c44751587d57425566ad5c308da906a
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-13 09:10:57 -0800

    WIP: add user interface logic to JS

---
+++ js/styles.js
@@ -0,0 +1,21 @@
+//Business Logic
+
+
+
+
+//User Interface Logic
+let form = document.querySelector("form");
+form.onsubmit = function(event) {
+  event.preventDefault();
+  const userInput = document.getElementById("celebrity").value;
+
+  console.log("submit form successful")
+
+  document.querySelector("#zodiac")
+
+
+}
+
+
+let result;
+  if 
