b2172148dfc84dad2fd1180201767d8cef821b5d
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-13 12:03:24 -0800

    WIP: assign values to answers and display/hide images for submit

--- js/styles.js
+++ js/styles.js
@@ -1,29 +1,30 @@
-let form = document.querySelector("form");
-form.onsubmit = function(event) {
-  event.preventDefault();
+window.onload = function() {
 
+  let form = document.querySelector("form");
 
-  var celebrity = document.querySelector("input#celebrity").value;
-  var zodiac = document.querySelector("input#zodiac").value;
-  var direction = document.querySelector("input#direction").value;
-  var genre = document.querySelector("input#genre").value;
+  form.onsubmit = function(event) {
+    event.preventDefault();
 
-  const userInput = document.getElementById("celebrity").value;
+    let celebrity = document.getElementById("celebrity");
+    let zodiac = document.getElementById("zodiac");
+    let direction = document.getElementById("direction");
+    let genre = document.getElementById("genre");
+    let color = document.getElementById("color");
 
-  console.log("submit form successful")
+    const userInput = document.getElementById("celebrity").value;
 
-if (celebrity === "N/A") {
-  alert("Pick a celebrity!")
+    if ((celebrity === "N/A") || (direction == "N/A") || (color === "N/A")) {
+      alert("INCOMPLETE!")
+    }
+
+    if ((celebrity === "Willem Dafoe") || (media === "movies") || (direction == "up") || (color = "purple")) {
+      form.removeAttribute("class");
+    } else if ((celebrity === "Beyonce") || (media === "Video Games") || (direction == "down") || (color === "pink")) {
+      document.getElementById("C#").show();
+    } else if ((celebrity === "Wendy Williams") && (media === "television") || (color === "red")) {
+      form.removeAttribute("class")
+    } else {
+      form.removeAttribute("class")
+    }
+  }
 }
-
-}
-
-
-  if (celebrity === "Willem Dafoe") || (media === "movies") || (direction == "left") || (color = "purple") {
-    document.getElementById("Python")
-  } else if (celebrity === "Beyonce") || (media === "Video Games") || (direction == "right") || (color === "pink") {
-    document.getElementById("C#").show();
-  } else if (celebrity === "Wendy Williams") && (media === "television") || (color === "red") {
-    document.getElementById("JavaScript")
-  } else 
-
