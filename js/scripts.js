8c9fb97fb4b5a24385ea83d0db05e68434f0d5f7
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-18 16:11:58 -0800

    redefine result variables, add onload function, update branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,40 @@
-function formSubmit(event) {
-  event.preventDefault();
-  // let python = document.getElementById("Python").value;
-  // let cSharp = document.getElementById("CSharp").value;
-  // let javaScript = document.getElementById("JavaScript").value;
-  let celebrityResult = document.getElementById("input#celebrity").value;
-  let mediaResult = document.getElementById("input#media").value;
-  let directionResult = document.getElementById("input#direction").value;
-  let genreResult = document.getElementById("input#genre").value;
-  let colorResult = document.getElementById("input#color").value;
-  const form = document.getElementById("results");
-
-console.log(celebrityResult + " - celeb");
-
-// function hideResults() {
-//   python.setAttribute("class", "hidden");
-//   cSharp.setAttribute("class", "hidden");
-//   javaScript.setAttribute("class", "hidden");
-// }
-// function showResults() {
-//   python.Element.removeAttribute("class", "hidden");
-//   cSharp.removeAttribute("class", "hidden");
-//   javaScript.removeAttribute("class", "hidden");
-// }
-
+function hideResults() {
+  python.setAttribute("class", "hidden");
+  cSharp.setAttribute("class", "hidden");
+  javaScript.setAttribute("class", "hidden");
+}
+function showResults(language) {
+  language.removeAttribute("class", "hidden");
+  // python.removeAttribute("class", "hidden");
+  // cSharp.removeAttribute("class", "hidden");
+  // javaScript.removeAttribute("class", "hidden");
+}
+window.onload = function(event) {
+let form = document.querySelector("form");
 
   form.onsubmit = function(event) {
+    console.log("on submit working")
     event.preventDefault();
-
+    let python = document.getElementById("Python")
+    let cSharp = document.getElementById("CSharp")
+    let javaScript = document.getElementById("JavaScript")
+    
+    const celebrityResult = document.getElementById("celebrity").value;
+    const mediaResult = document.getElementById("media").value;
+    const directionResult = document.getElementById("direction").value;
+    const genreResult = document.getElementById("genre").value;
+    const colorResult = document.getElementById("color").value;
 
     if (celebrityResult && mediaResult && directionResult && genreResult && colorResult) {
-      if ((celebrityResult === "robert") && (mediaResult === "television") && (directionResult === "nowhere") && (genreResult === "iop" ) || (colorResult === "green")) {
+      if ((celebrityResult === "robert") || (mediaResult === "television") || (directionResult === "nowhere") || (genreResult === "iop" ) || (colorResult === "green")) {
         showResults(python);
-      } else if ((celebrityResult === "beyonce") && (mediaResult === "books") && (directionResult === "up") && (genreResult === "shoegaze" ) || (colorResult === "red")) {
+      } else if ((celebrityResult === "beyonce") || (mediaResult === "books") || (directionResult === "up") || (genreResult === "shoegaze" ) || (colorResult === "red")) {
         showResults(cSharp);
-      } else if ((celebrityResult === "willem") && (mediaResult === "videogames") && (directionResult === "down") && (genreResult === "horror-country" ) || (colorResult === "purple")) { 
-        showResults(JavaScript);
+      } else if ((celebrityResult === "willem") || (mediaResult === "videogames") || (directionResult === "down") || (genreResult === "horror-country" ) || (colorResult === "purple")) { 
+        showResults(javaScript);
       } else {
-        showResults(python);
+        showResults(cSharp)
       };
     };
   };
 }
-
-  window.addEventListener("load", function() {
-    results.addEventListener("submit", formSubmit);
-  })
-  
-
-
